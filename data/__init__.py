bcb73652f2911ecaeda7bdfeb089d210452252f5
lavinal712 <austin712@mail.ustc.edu.cn>
2024-08-21 20:33:46 +0800

    update data/__init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -41,7 +41,7 @@ class CustomDataset(Dataset):
         feature_file = self.features_files[idx]
 
         label_idx = int(os.path.splitext(feature_file)[0].split("-")[0])
-        caption_file = np.random.choice(filter(lambda x: x.startswith(f"{label_idx}-"), self.captions_files))
+        caption_file = np.random.choice(list(filter(lambda x: x.startswith(f"{label_idx}-"), self.captions_files)))
         mask_file = caption_file
 
         features = np.load(os.path.join(self.features_dir, feature_file))
